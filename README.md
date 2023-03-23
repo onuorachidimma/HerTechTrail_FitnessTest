52c1988b85414e528979a5df9436bb5955495665
Chidimma Onuora <onuorachidimma4@gmail.com>
2023-03-23 23:12:56 +0100

    files update

---
+++ README.md
@@ -0,0 +1,8 @@
+This is the final product of my fitness test for HerTechTrail Front-end web development pro track.
+
+Fitness test Question:
+Create a picture gallery and add a name under every picture in the gallery. On clicking on each name, the name and background should change to different colors.
+
+No picture should be the same in the gallery.
+
+TECH: HTML, CSS and Javascript.
---
+++ script.js
@@ -0,0 +1,95 @@
+const transform = document.getElementById("image1")
+transform.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const transform2 = document.getElementById("image2")
+transform2.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const transform3 = document.getElementById("image3")
+transform3.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const transform4 = document.getElementById("image4")
+transform4.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const transform5 = document.getElementById("image5")
+transform5.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const transform6 = document.getElementById("image6")
+transform6.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const transform7 = document.getElementById("image7")
+transform7.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const transform8 = document.getElementById("image8")
+transform8.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const transform9 = document.getElementById("image9")
+transform9.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const transform10 = document.getElementById("image10")
+transform10.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const transform11 = document.getElementById("image11")
+transform11.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const transform12 = document.getElementById("image12")
+transform12.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const transform13 = document.getElementById("image13")
+transform13.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const transform14 = document.getElementById("image14")
+transform14.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const transform15 = document.getElementById("image15")
+transform15.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const transform16 = document.getElementById("image16")
+transform16.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
